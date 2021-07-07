1e8ef31fec0b295da1c2d43ebf23d82f2d4eae5d
llev <llev1@me.com>
2021-07-07 10:24:36 +0100

    Added a contact page

---
+++ web/src/routes/contact.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>Contact</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+  <a href="https://github.com/llev" target="_blank">Follow Me on Github 👨🏻</a>
